df8068c5575bfc0a7718fde9126aff753d72f432
Andikat Jacob Dennis <andikatjacobdennis@gmail.com>
2025-07-04 16:23:02 +0530

    Update Sample.ts

--- Sample.ts
+++ Sample.ts
@@ -1,1 +1,28 @@
+interface User {
+  id: number;
+  name: string;
+}
+
+enum Role {
+  Admin,
+  Guest
+}
+
+type Result<T> = T | null;
+
+function globalFunc(x: number): void {
+  console.log(x);
+}
+
+const inline = (msg: string) => console.log(msg);
+
+class Greeter {
+  greet(name: string): string {
+    return `Hello, ${name}`;
+  }
+
+  shout = (text: string) => {
+    return text.toUpperCase();
+  };
+}
 
